87aed3b9191a03bf1abd97e9ee32317e67708006
Simon Fuet <simonfuet@gmail.com>
2019-12-23 14:23:23 +0100

    IoC xml

--- spring-initiation/ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
+++ spring-initiation/ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
@@ -1,10 +1,11 @@
 package org.example.demo.ticket.batch;
 
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.example.demo.ticket.business.contract.ManagerFactory;
 import org.example.demo.ticket.model.exception.TechnicalException;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Classe Principale de lancement des Batches.
@@ -13,32 +14,35 @@ import org.example.demo.ticket.model.exception.TechnicalException;
  */
 public class Main {
 
-    /** Logger pour la classe */
-    private static final Log LOGGER = LogFactory.getLog(Main.class);
+	/** Logger pour la classe */
+	private static final Log LOGGER = LogFactory.getLog(Main.class);
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param pArgs the input arguments
+	 * @throws TechnicalException sur erreur technique
+	 */
+	public static void main(String[] pArgs) throws TechnicalException {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
 
-    /**
-     * The entry point of application.
-     *
-     * @param pArgs the input arguments
-     * @throws TechnicalException sur erreur technique
-     */
-    public static void main(String[] pArgs) throws TechnicalException {
-        try {
-            if (pArgs.length < 1) {
-                throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
-            }
+		ManagerFactory managerFactory = applicationContext.getBean("managerFactory", ManagerFactory.class);
 
-            String vTraitementId = pArgs[0];
-            if ("ExportTicketStatus".equals(vTraitementId)) {
-                LOGGER.info("Execution du traitement : ExportTicketStatus");
-                // ...
-            } else {
-                throw new TechnicalException("Traitement inconnu : " + vTraitementId);
-            }
-        } catch (Throwable vThrowable) {
-            LOGGER.error(vThrowable);
-            System.exit(1);
-        }
-    }
+		try {
+			if (pArgs.length < 1) {
+				throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
+			}
+
+			String vTraitementId = pArgs[0];
+			if ("ExportTicketStatus".equals(vTraitementId)) {
+				LOGGER.info("Execution du traitement : ExportTicketStatus");
+				// ...
+			} else {
+				throw new TechnicalException("Traitement inconnu : " + vTraitementId);
+			}
+		} catch (Throwable vThrowable) {
+			LOGGER.error(vThrowable);
+			System.exit(1);
+		}
+	}
 }
