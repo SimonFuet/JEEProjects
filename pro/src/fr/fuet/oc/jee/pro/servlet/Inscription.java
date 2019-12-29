4393f9a0ccdad54bdb0845557997f97ceec31104
Simon Fuet <simonfuet@gmail.com>
2019-12-29 14:13:48 +0100

    JEE forumlaire MVC

--- pro/src/fr/fuet/oc/jee/pro/servlet/Inscription.java
+++ pro/src/fr/fuet/oc/jee/pro/servlet/Inscription.java
@@ -1,101 +1,41 @@
 package fr.fuet.oc.jee.pro.servlet;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.fuet.oc.jee.pro.bean.Utilisateur;
+import fr.fuet.oc.jee.pro.form.InscriptionForm;
+
 public class Inscription extends HttpServlet {
+	public static final String ATT_USER = "utilisateur";
+	public static final String ATT_FORM = "form";
 	public static final String VUE = "/WEB-INF/inscription.jsp";
-	public static final String CHAMP_EMAIL = "email";
-	public static final String CHAMP_PASS = "motdepasse";
-	public static final String CHAMP_CONF = "confirmation";
-	public static final String CHAMP_NOM = "nom";
-	public static final String ATT_ERREURS = "erreurs";
-	public static final String ATT_RESULTAT = "resultat";
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Affichage de la page d'inscription */
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String resultat;
-		Map<String, String> erreurs = new HashMap<>();
-		String email = request.getParameter(CHAMP_EMAIL);
-		String motDePasse = request.getParameter(CHAMP_PASS);
-		String confirmation = request.getParameter(CHAMP_CONF);
-		String nom = request.getParameter(CHAMP_NOM);
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Préparation de l'objet formulaire */
+		InscriptionForm form = new InscriptionForm();
 
-		try {
-			validationEmail(email);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_EMAIL, e.getMessage());
-		}
-		try {
-			validationMotsDePasse(motDePasse, confirmation);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_PASS, e.getMessage());
-		}
-		try {
-			validationNom(nom);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_NOM, e.getMessage());
-		}
+		/*
+		 * Appel au traitement et à la validation de la requête, et récupération du bean
+		 * en résultant
+		 */
+		Utilisateur utilisateur = form.inscrireUtilisateur(request);
 
-		if (erreurs.isEmpty()) {
-			resultat = "Succès de l'inscription.";
-		} else {
-			resultat = "Échec de l'inscription.";
-		}
-
-		request.setAttribute(ATT_RESULTAT, resultat);
-		request.setAttribute(ATT_ERREURS, erreurs);
+		/* Stockage du formulaire et du bean dans l'objet request */
+		request.setAttribute(ATT_FORM, form);
+		request.setAttribute(ATT_USER, utilisateur);
 
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
-	}
-
-	/**
-	 * Valide l'adresse mail saisie.
-	 */
-	private void validationEmail(String email) throws Exception {
-		if (email != null && email.trim().length() != 0) {
-			if (!email.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
-				throw new Exception("Merci de saisir une adresse mail valide.");
-			}
-		} else {
-			throw new Exception("Merci de saisir une adresse mail.");
-		}
-	}
-
-	/**
-	 * Valide les mots de passe saisis.
-	 */
-	private void validationMotsDePasse(String motDePasse, String confirmation) throws Exception {
-		if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null
-				&& confirmation.trim().length() != 0) {
-			if (!motDePasse.equals(confirmation)) {
-				throw new Exception("Les mots de passe entrés sont différents, merci de les saisir à nouveau.");
-			} else if (motDePasse.trim().length() < 3) {
-				throw new Exception("Les mots de passe doivent contenir au moins 3 caractères.");
-			}
-		} else {
-			throw new Exception("Merci de saisir et confirmer votre mot de passe.");
-		}
-	}
-
-	/**
-	 * Valide le nom d'utilisateur saisi.
-	 */
-	private void validationNom(String nom) throws Exception {
-		if (nom != null && !nom.isEmpty() && nom.trim().length() < 3) {
-			throw new Exception("Le nom d'utilisateur doit contenir au moins 3 caractères.");
-		}
 	}
 }
