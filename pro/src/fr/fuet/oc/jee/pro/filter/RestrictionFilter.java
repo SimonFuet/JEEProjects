cd2ea58f94718702747ff883bad38ad679df452d
Simon Fuet <simonfuet@gmail.com>
2020-02-25 20:07:10 +0100

    Filter set of pages

--- pro/src/fr/fuet/oc/jee/pro/filter/RestrictionFilter.java
+++ pro/src/fr/fuet/oc/jee/pro/filter/RestrictionFilter.java
@@ -1,24 +1,36 @@
-package fr.fuet.oc.jee.pro.servlet;
+package fr.fuet.oc.jee.pro.filter;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class Restriction extends HttpServlet {
+public class RestrictionFilter implements Filter {
 	public static final String ACCES_PUBLIC = "/accesPublic.jsp";
-	public static final String ACCES_RESTREINT = "/WEB-INF/accesRestreint.jsp";
 	public static final String ATT_SESSION_USER = "sessionUtilisateur";
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		/* Cast des objets request et response */
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
 		/* Récupération de la session depuis la requête */
 		HttpSession session = request.getSession();
 
-		/*
+		/**
 		 * Si l'objet utilisateur n'existe pas dans la session en cours, alors
 		 * l'utilisateur n'est pas connecté.
 		 */
@@ -27,7 +39,11 @@ public class Restriction extends HttpServlet {
 			response.sendRedirect(request.getContextPath() + ACCES_PUBLIC);
 		} else {
 			/* Affichage de la page restreinte */
-			this.getServletContext().getRequestDispatcher(ACCES_RESTREINT).forward(request, response);
+			chain.doFilter(request, response);
 		}
+	}
+
+	@Override
+	public void destroy() {
 	}
 }
