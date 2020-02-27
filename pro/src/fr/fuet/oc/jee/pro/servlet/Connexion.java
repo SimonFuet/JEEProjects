ce91350a50577f963593c7cdf779af15306ba8eb
Simon Fuet <simonfuet@gmail.com>
2020-02-27 10:36:35 +0100

    Cookies

--- pro/src/fr/fuet/oc/jee/pro/servlet/Connexion.java
+++ pro/src/fr/fuet/oc/jee/pro/servlet/Connexion.java
@@ -1,12 +1,23 @@
 package fr.fuet.oc.jee.pro.servlet;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import fr.fuet.oc.jee.pro.bean.Utilisateur;
 import fr.fuet.oc.jee.pro.form.ConnexionForm;
@@ -14,11 +25,36 @@ import fr.fuet.oc.jee.pro.form.ConnexionForm;
 public class Connexion extends HttpServlet {
 	public static final String ATT_USER = "utilisateur";
 	public static final String ATT_FORM = "form";
+	public static final String ATT_INTERVALLE_CONNEXIONS = "intervalleConnexions";
 	public static final String ATT_SESSION_USER = "sessionUtilisateur";
+	public static final String COOKIE_DERNIERE_CONNEXION = "derniereConnexion";
+	public static final String FORMAT_DATE = "dd/MM/yyyy HH:mm:ss";
 	public static final String VUE = "/WEB-INF/connexion.jsp";
+	public static final String CHAMP_MEMOIRE = "memoire";
+	public static final int COOKIE_MAX_AGE = 60 * 60 * 24 * 365; // 1 an
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Tentative de récupération du cookie depuis la requête */
+		String derniereConnexion = getCookieValue(request, COOKIE_DERNIERE_CONNEXION);
+		/* Si le cookie existe, alors calcul de la durée */
+		if (derniereConnexion != null) {
+			/* Récupération de la date courante */
+			DateTime dtCourante = new DateTime();
+			/* Récupération de la date présente dans le cookie */
+			DateTimeFormatter formatter = DateTimeFormat.forPattern(FORMAT_DATE);
+			DateTime dtDerniereConnexion = formatter.parseDateTime(derniereConnexion);
+			/* Calcul de la durée de l'intervalle */
+			Period periode = new Period(dtDerniereConnexion, dtCourante);
+			/* Formatage de la durée de l'intervalle */
+			PeriodFormatter periodFormatter = new PeriodFormatterBuilder().appendYears().appendSuffix(" an ", " ans ")
+					.appendMonths().appendSuffix(" mois ").appendDays().appendSuffix(" jour ", " jours ").appendHours()
+					.appendSuffix(" heure ", " heures ").appendMinutes().appendSuffix(" minute ", " minutes ")
+					.appendSeparator("et ").appendSeconds().appendSuffix(" seconde", " secondes").toFormatter();
+			String intervalleConnexions = periodFormatter.print(periode);
+			/* Ajout de l'intervalle en tant qu'attribut de la requête */
+			request.setAttribute(ATT_INTERVALLE_CONNEXIONS, intervalleConnexions);
+		}
 		/* Affichage de la page de connexion */
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
@@ -27,14 +63,12 @@ public class Connexion extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		/* Préparation de l'objet formulaire */
 		ConnexionForm form = new ConnexionForm();
-
 		/* Traitement de la requête et récupération du bean en résultant */
 		Utilisateur utilisateur = form.connecterUtilisateur(request);
-
 		/* Récupération de la session depuis la requête */
 		HttpSession session = request.getSession();
 
-		/**
+		/*
 		 * Si aucune erreur de validation n'a eu lieu, alors ajout du bean Utilisateur à
 		 * la session, sinon suppression du bean de la session.
 		 */
@@ -44,10 +78,48 @@ public class Connexion extends HttpServlet {
 			session.setAttribute(ATT_SESSION_USER, null);
 		}
 
+		/* Si et seulement si la case du formulaire est cochée */
+		if (request.getParameter(CHAMP_MEMOIRE) != null) {
+			/* Récupération de la date courante */
+			DateTime dt = new DateTime();
+			/* Formatage de la date et conversion en texte */
+			DateTimeFormatter formatter = DateTimeFormat.forPattern(FORMAT_DATE);
+			String dateDerniereConnexion = dt.toString(formatter);
+			/* Création du cookie, et ajout à la réponse HTTP */
+			setCookie(response, COOKIE_DERNIERE_CONNEXION, dateDerniereConnexion, COOKIE_MAX_AGE);
+		} else {
+			/* Demande de suppression du cookie du navigateur */
+			setCookie(response, COOKIE_DERNIERE_CONNEXION, "", 0);
+		}
+
 		/* Stockage du formulaire et du bean dans l'objet request */
 		request.setAttribute(ATT_FORM, form);
 		request.setAttribute(ATT_USER, utilisateur);
 
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	private static String getCookieValue(HttpServletRequest request, String name) throws UnsupportedEncodingException {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie != null && name.equals(cookie.getName())) {
+					return URLDecoder.decode(cookie.getValue(), "UTF-8");
+				}
+			}
+		}
+
+		return null;
+	}
+
+	/*
+	 * Méthode utilitaire gérant la création d'un cookie et son ajout à la réponse
+	 * HTTP.
+	 */
+	private static void setCookie(HttpServletResponse response, String nom, String valeur, int maxAge)
+			throws UnsupportedEncodingException {
+		Cookie cookie = new Cookie(nom, URLEncoder.encode(valeur, "UTF-8"));
+		cookie.setMaxAge(maxAge);
+		response.addCookie(cookie);
 	}
 }
