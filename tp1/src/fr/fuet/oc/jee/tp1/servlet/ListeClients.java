bb3918f2cae4295eb6712ad2dcf79f65cdcdb1ab
Simon Fuet <simonfuet@gmail.com>
2020-03-06 22:39:09 +0100

    JEE TP4 Session

---
+++ tp1/src/fr/fuet/oc/jee/tp1/servlet/ListeClients.java
@@ -0,0 +1,20 @@
+package fr.fuet.oc.jee.tp1.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListeClients extends HttpServlet {
+	public static final String ATT_CLIENT = "client";
+	public static final String ATT_FORM = "form";
+
+	public static final String VUE = "/WEB-INF/listerClients.jsp";
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+}
